2b96969ad56bcb7bd0218682a7bad95ac5fd13fd
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-01 23:44:01 +0300

    fix celery issues

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -8,43 +8,56 @@ logger = logging.getLogger(__name__)
 
 @shared_task()
 def send_notification(event_pk):
-    event = Event.objects.get(pk=event_pk)
-    logger.info(f'ID{event.pk} - Sending notification')
-    logger.info(f'{event.date} {event.time}')
+    try:
+        event = Event.objects.get(pk=event_pk)
+        logger.info(f'ID{event.pk} - Sending notification')
+        logger.info(f'{event.date} {event.time}')
+    except Exception as e:
+        logger.error(e)
 
 
-def get_new_date_and_time(old_date, old_time, interval, current_datetime):
-    datetime_str = f'{old_date} {old_time}'
-    datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M').replace(tzinfo=timezone.utc)
-    while datetime_object < current_datetime:
-        datetime_object += timedelta(**parse_notice_time_or_interval(interval))
-    datetime_str = datetime.strftime(datetime_object, '%Y-%m-%d %H:%M')
-    new_date = datetime_str[:10]
-    new_time = datetime_str[-5:]
-    return new_date, new_time
+def get_new_date_and_time(old_date, old_time, interval, current_utc_timestamp):
+    current_datetime = datetime.fromtimestamp(current_utc_timestamp, tz=timezone.utc)
+    try:
+        datetime_str = f'{old_date} {old_time}'
+        datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M').replace(tzinfo=timezone.utc)
+        while datetime_object < current_datetime:
+            datetime_object += timedelta(**parse_notice_time_or_interval(interval))
+        datetime_str = datetime.strftime(datetime_object, '%Y-%m-%d %H:%M')
+        new_date = datetime_str[:10]
+        new_time = datetime_str[-5:]
+        return new_date, new_time
+    except Exception as e:
+        logger.error(e)
 
 
 @shared_task()
-def reschedule_or_delete_event(event_pk, current_datetime):
-    event = Event.objects.get(pk=event_pk)
-    if event.interval == '-':
-        logger.info(f'ID{event.pk} - Deleting')
-        event.delete()
-    else:
-        logger.info(f'ID{event.pk} - Rescheduling')
-        new_date, new_time = get_new_date_and_time(event.date, event.time, event.interval, current_datetime)
-        event.date = new_date
-        event.time = new_time
-        logger.info(f'New: {event.date} {event.time}')
-        event.save()
+def reschedule_or_delete_event(event_pk, current_utc_timestamp):
+    try:
+        event = Event.objects.get(pk=event_pk)
+        if event.interval == '-':
+            logger.info(f'ID{event.pk} - Deleting')
+            event.delete()
+        else:
+            logger.info(f'ID{event.pk} - Rescheduling')
+            new_date, new_time = get_new_date_and_time(event.date, event.time, event.interval, current_utc_timestamp)
+            event.date = new_date
+            event.time = new_time
+            logger.info(f'New: {event.date} {event.time}')
+            event.save()
+    except Exception as e:
+        logger.error(e)
 
 
 @shared_task()
 def heartbeat():
-    current_datetime = datetime.now(timezone.utc)
-    current_utc_timestamp = int(current_datetime.timestamp())
-    logger.info(f'HEARTBEAT. UTC: {current_utc_timestamp}')
-    expired_events = Event.objects.filter(utc_timestamp__lt=current_utc_timestamp)
-    for event in expired_events:
-        logger.info(f'ID{event.pk} {event.title}({event.type}) - Expired')
-        chain(send_notification.delay(event.pk) | reschedule_or_delete_event.delay(event.pk, current_datetime))
+    try:
+        current_datetime = datetime.now(timezone.utc)
+        current_utc_timestamp = int(current_datetime.timestamp())
+        logger.info(f'HEARTBEAT. UTC: {current_utc_timestamp}')
+        expired_events = Event.objects.filter(utc_timestamp__lt=current_utc_timestamp)
+        for event in expired_events:
+            logger.info(f'ID{event.pk} {event.title}({event.type}) - Expired')
+            chain(send_notification.delay(event.pk) | reschedule_or_delete_event.delay(event.pk, current_utc_timestamp))
+    except Exception as e:
+        logger.error(e)
--- backend/core/tests/test_tasks.py
+++ backend/core/tests/test_tasks.py
@@ -9,21 +9,22 @@ class TestTasks(TestCase):
 
     def setUp(self):
         self.current_datetime = datetime(2024, 1, 1, 10, 5, tzinfo=timezone.utc)  # 2024-01-01 10:05
+        self.current_timestamp = int(self.current_datetime.timestamp())
 
     def test_get_new_date_and_time(self):
-        result = get_new_date_and_time('2024-01-01', '10:00', '30min', self.current_datetime)
+        result = get_new_date_and_time('2024-01-01', '10:00', '30min', self.current_timestamp)
         expected_result = ('2024-01-01', '10:30')
         self.assertEqual(result, expected_result)
 
     def test_reschedule_or_delete_event_without_interval(self):
         event = Event.objects.create(title=f'Title-1', date='2024-01-01', time='10:00')
         self.assertEqual(Event.objects.count(), 1)
-        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be deleted
+        reschedule_or_delete_event(event.pk, self.current_timestamp)  # Event should be deleted
         self.assertEqual(Event.objects.count(), 0)
 
     def test_reschedule_or_delete_event_with_interval(self):
         event = Event.objects.create(title=f'Title-1', date='2024-01-01', time='10:00', interval='30min')
-        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be updated
+        reschedule_or_delete_event(event.pk, self.current_timestamp)  # Event should be updated
         self.assertEqual(Event.objects.count(), 1)
         self.assertEqual(Event.objects.get().title, "Title-1")
         self.assertEqual(Event.objects.get().date, "2024-01-01")
@@ -31,8 +32,37 @@ class TestTasks(TestCase):
 
     def test_reschedule_or_delete_event_with_interval_missed_timestamp(self):
         event = Event.objects.create(title=f'Title-1', date='2023-01-01', time='10:00', interval='30min')
-        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be updated
+        reschedule_or_delete_event(event.pk, self.current_timestamp)  # Event should be updated
         self.assertEqual(Event.objects.count(), 1)
         self.assertEqual(Event.objects.get().title, "Title-1")
         self.assertEqual(Event.objects.get().date, "2024-01-01")
         self.assertEqual(Event.objects.get().time, "10:30")
+
+    # @patch('core.tasks.current_datetime', return_value=datetime(2024, 1, 2, 10, 0, tzinfo=timezone.utc))
+    # @patch('core.tasks.heartbeat.datetime.now', return_value=datetime(2024, 1, 2, 10, 0, tzinfo=timezone.utc))
+    # def test_heartbeat_with_interval(self):
+    #     Event.objects.create(title=f'Title-1', date='2022-01-01', interval='30min')
+    #     # with patch('celeryconfig.CELERY_ALWAYS_EAGER', True, create=True):
+    #     heartbeat.apply()
+    #     self.assertEqual(Event.objects.count(), 1)
+    #     self.assertEqual(Event.objects.get().title, "Title-1")
+    #     self.assertEqual(Event.objects.get().date, "2022-01-01")
+    #     self.assertEqual(Event.objects.get().time, "10:30")
+
+
+# class TestHeartbeat:
+#     @pytest.fixture(scope='session')
+#     def celery_config():
+#         return {
+#             'broker_url': 'amqp://',
+#             'result_backend': 'redis://'
+#         }
+
+    # @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True, CELERY_ALWAYS_EAGER=True, BROKER_BACKEND='memory')
+    # def test_heartbeat_with_interval(self):
+    #     Event.objects.create(title=f'Title-1', date='2022-01-01', interval='30min')
+    #     heartbeat()
+    #     self.assertEqual(Event.objects.count(), 1)
+    #     self.assertEqual(Event.objects.get().title, "Title-1")
+    #     self.assertEqual(Event.objects.get().date, "2022-01-01")
+    #     self.assertEqual(Event.objects.get().time, "10:30")

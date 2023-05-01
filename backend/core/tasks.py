93f36322d7aa709a3d4923fd7ba1bf25357cd06a
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-01 22:53:14 +0300

    chain event processing tasks; add task tests

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from celery import shared_task, chain
 from core.models import Event, parse_notice_time_or_interval
 from datetime import datetime, timezone, timedelta
 import logging
@@ -10,12 +10,14 @@ logger = logging.getLogger(__name__)
 def send_notification(event_pk):
     event = Event.objects.get(pk=event_pk)
     logger.info(f'ID{event.pk} - Sending notification')
+    logger.info(f'{event.date} {event.time}')
 
 
-def get_new_date_and_time(old_date, old_time, interval):
+def get_new_date_and_time(old_date, old_time, interval, current_datetime):
     datetime_str = f'{old_date} {old_time}'
-    datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M')
-    datetime_object += timedelta(**parse_notice_time_or_interval(interval))
+    datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M').replace(tzinfo=timezone.utc)
+    while datetime_object < current_datetime:
+        datetime_object += timedelta(**parse_notice_time_or_interval(interval))
     datetime_str = datetime.strftime(datetime_object, '%Y-%m-%d %H:%M')
     new_date = datetime_str[:10]
     new_time = datetime_str[-5:]
@@ -23,16 +25,17 @@ def get_new_date_and_time(old_date, old_time, interval):
 
 
 @shared_task()
-def reschedule_or_delete_event(event_pk):
+def reschedule_or_delete_event(event_pk, current_datetime):
     event = Event.objects.get(pk=event_pk)
     if event.interval == '-':
         logger.info(f'ID{event.pk} - Deleting')
         event.delete()
     else:
         logger.info(f'ID{event.pk} - Rescheduling')
-        new_date, new_time = get_new_date_and_time(event.date, event.time, event.interval)
+        new_date, new_time = get_new_date_and_time(event.date, event.time, event.interval, current_datetime)
         event.date = new_date
         event.time = new_time
+        logger.info(f'New: {event.date} {event.time}')
         event.save()
 
 
@@ -44,5 +47,4 @@ def heartbeat():
     expired_events = Event.objects.filter(utc_timestamp__lt=current_utc_timestamp)
     for event in expired_events:
         logger.info(f'ID{event.pk} {event.title}({event.type}) - Expired')
-        send_notification.delay(event.pk)
-        reschedule_or_delete_event.delay(event.pk)
+        chain(send_notification.delay(event.pk) | reschedule_or_delete_event.delay(event.pk, current_datetime))
---
+++ backend/core/tests/test_tasks.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from core.tasks import get_new_date_and_time, reschedule_or_delete_event, heartbeat
+from core.models import Event
+from datetime import datetime, timezone
+
+
+class TestTasks(TestCase):
+    """ Test suite for Celery tasks """
+
+    def setUp(self):
+        self.current_datetime = datetime(2024, 1, 1, 10, 5, tzinfo=timezone.utc)  # 2024-01-01 10:05
+
+    def test_get_new_date_and_time(self):
+        result = get_new_date_and_time('2024-01-01', '10:00', '30min', self.current_datetime)
+        expected_result = ('2024-01-01', '10:30')
+        self.assertEqual(result, expected_result)
+
+    def test_reschedule_or_delete_event_without_interval(self):
+        event = Event.objects.create(title=f'Title-1', date='2024-01-01', time='10:00')
+        self.assertEqual(Event.objects.count(), 1)
+        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be deleted
+        self.assertEqual(Event.objects.count(), 0)
+
+    def test_reschedule_or_delete_event_with_interval(self):
+        event = Event.objects.create(title=f'Title-1', date='2024-01-01', time='10:00', interval='30min')
+        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be updated
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.get().title, "Title-1")
+        self.assertEqual(Event.objects.get().date, "2024-01-01")
+        self.assertEqual(Event.objects.get().time, "10:30")
+
+    def test_reschedule_or_delete_event_with_interval_missed_timestamp(self):
+        event = Event.objects.create(title=f'Title-1', date='2023-01-01', time='10:00', interval='30min')
+        reschedule_or_delete_event(event.pk, self.current_datetime)  # Event should be updated
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.get().title, "Title-1")
+        self.assertEqual(Event.objects.get().date, "2024-01-01")
+        self.assertEqual(Event.objects.get().time, "10:30")

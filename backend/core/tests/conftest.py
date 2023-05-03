5eb72eb3183f1b2fa99e0f08e03c5e58b385d25d
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-04 00:09:50 +0300

    integrate pytest

---
+++ backend/core/tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture(scope='session')
+def celery_config():
+    return {
+        'broker_url': 'memory://',
+        'result_backend': 'redis://',
+        'worker_concurrency': 1
+    }

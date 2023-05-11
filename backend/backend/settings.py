1fb16c9e839d5e4de84c01b040bcda17a0908709
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-11 19:58:00 +0300

    add send_email; send_windows_popup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,5 +177,5 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT", 465)
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = True
--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -2,8 +2,46 @@ from celery import shared_task, chain
 from core.models import Event, parse_notice_time_or_interval
 from datetime import datetime, timezone, timedelta
 import logging
+from django.core.mail import send_mail
 
 logger = logging.getLogger(__name__)
+
+
+@shared_task()
+def send_windows_popup(title, text):  # For local development only
+    import ctypes
+    import winsound
+    winsound.Beep(440, 1000)  # 440Hz, 1000ms
+    ctypes.windll.user32.MessageBoxW(0, text, title, 0)
+    return None
+
+
+@shared_task()
+def send_email(title, text):
+    try:
+        send_mail(
+            title,
+            text,
+            None,
+            ["recipient@email.com"],  # Todo: Sync with user settings
+            fail_silently=False,
+        )
+        return None
+    except Exception as e:
+        logger.exception(e)
+        raise
+
+
+def build_notification_text(event):
+    notification_text = f'It is time for {event.title} ({event.type})'
+    if event.notice_time != '-':
+        notification_text += f' in {event.notice_time}'
+    notification_text += '.'
+    if event.interval != '-':
+        notification_text += f' Next such event scheduled in {event.interval}.'
+    if event.info:
+        notification_text += f' Info: {event.info}'
+    return notification_text
 
 
 @shared_task()
@@ -12,6 +50,10 @@ def send_notification(event_pk):
         event = Event.objects.get(pk=event_pk)
         logger.info(f'{event} - Sending notification')
         logger.info(f'{event.date} {event.time}')
+        notification_title = f'Time for {event.title}({event.type})!'
+        notification_text = build_notification_text(event)
+        # send_windows_popup.delay(notification_title, notification_text)
+        send_email.delay(notification_title, notification_text)
         return None
     except Exception as e:
         logger.exception(e)

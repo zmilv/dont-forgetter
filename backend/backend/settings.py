1f4ec6faab73685e6cfa72a3a93b278f447d8eb9
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-26 17:12:22 +0300

    Revert "add encryption"
    This reverts commit aaebf8a15d1ee4e5f43493b381a9edd9eb9504c8.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -227,6 +227,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
-CRYPTOGRAPHY_KEY = os.environ.get("CRYPTOGRAPHY_KEY", None)
-CRYPTOGRAPHY_SALT = os.environ.get("CRYPTOGRAPHY_SALT", "default_salt")

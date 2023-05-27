9e3e13d08176d6b8d4dd97622cc369329112dae9
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-05-27 13:21:30 +0300

    add separate docker-compose for pipeline; run tests inside container; uncomment celery integration tests

--- backend/core/tests/test_tasks.py
+++ backend/core/tests/test_tasks.py
@@ -75,101 +75,101 @@ class TestTasks(TestCase):
         self.assertEqual(Event.objects.get().time, "10:30")
 
 
-# @freeze_time("2020-01-01 10:05")  # Mocks current datetime
-# class TestCeleryIntegration(SimpleTestCase):
-#     databases = "__all__"
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#         cls.user = CustomUser.objects.create_user(
-#             email="email@email.com", username="name", password=make_password("password")
-#         )
-#         # Start celery worker
-#         cls.celery_worker = start_worker(app, perform_ping_check=False)
-#         cls.celery_worker.__enter__()
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         super().tearDownClass()
-#         # Close worker
-#         cls.celery_worker.__exit__(None, None, None)
-#
-#     def _post_teardown(self):
-#         # Clear Event instances after each test
-#         Event.objects.all().delete()
-#
-#     def test_heartbeat_without_interval(self):
-#         Event.objects.create(
-#             title=f"Title-1", date="2020-01-01", time="10:00", user=self.user
-#         )
-#         self.assertEqual(Event.objects.count(), 1)
-#         heartbeat_task = heartbeat.delay()  # Event should be deleted
-#         result = heartbeat_task.get()
-#         reschedule_or_delete_id, send_notification_id = result[0], result[1]
-#         send_notification_task = AsyncResult(send_notification_id, app=app)
-#         reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
-#         send_notification_task.get()
-#         reschedule_or_delete_task.get()
-#         self.assertEqual(heartbeat_task.status, "SUCCESS")
-#         self.assertEqual(send_notification_task.status, "SUCCESS")
-#         self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
-#         self.assertEqual(Event.objects.count(), 0)
-#
-#     def test_heartbeat_with_interval(self):
-#         Event.objects.create(
-#             title=f"Title-1",
-#             date="2020-01-01",
-#             time="10:00",
-#             interval="30min",
-#             user=self.user,
-#         )
-#         self.assertEqual(Event.objects.count(), 1)
-#         heartbeat_task = heartbeat.delay()  # Event should be updated
-#         result = heartbeat_task.get()
-#         reschedule_or_delete_id, send_notification_id = result[0], result[1]
-#         send_notification_task = AsyncResult(send_notification_id, app=app)
-#         reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
-#         send_notification_task.get()
-#         reschedule_or_delete_task.get()
-#         self.assertEqual(heartbeat_task.status, "SUCCESS")
-#         self.assertEqual(send_notification_task.status, "SUCCESS")
-#         self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
-#         self.assertEqual(Event.objects.count(), 1)
-#         self.assertEqual(Event.objects.get().date, "2020-01-01")
-#         self.assertEqual(Event.objects.get().time, "10:30")
-#
-#     def test_heartbeat_with_missed_timestamp(self):
-#         Event.objects.create(
-#             title=f"Title-1",
-#             date="2019-01-01",
-#             time="10:00",
-#             interval="30min",
-#             user=self.user,
-#         )
-#         self.assertEqual(Event.objects.count(), 1)
-#         heartbeat_task = heartbeat.delay()  # Event should be updated
-#         result = heartbeat_task.get()
-#         reschedule_or_delete_id, send_notification_id = result[0], result[1]
-#         send_notification_task = AsyncResult(send_notification_id, app=app)
-#         reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
-#         send_notification_task.get()
-#         reschedule_or_delete_task.get()
-#         self.assertEqual(heartbeat_task.status, "SUCCESS")
-#         self.assertEqual(send_notification_task.status, "SUCCESS")
-#         self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
-#         self.assertEqual(Event.objects.count(), 1)
-#         self.assertEqual(Event.objects.get().date, "2020-01-01")
-#         self.assertEqual(Event.objects.get().time, "10:30")
-#
-#     def test_heartbeat_without_expired_events(self):
-#         Event.objects.create(
-#             title=f"Title-1", date="2020-01-01", time="11:00", user=self.user
-#         )
-#         self.assertEqual(Event.objects.count(), 1)
-#         heartbeat_task = heartbeat.delay()  # Event should remain unchanged
-#         result = heartbeat_task.get()
-#         self.assertEqual(result, [])
-#         self.assertEqual(Event.objects.count(), 1)
-#         self.assertEqual(Event.objects.get().date, "2020-01-01")
-#         self.assertEqual(Event.objects.get().time, "11:00")
+@freeze_time("2020-01-01 10:05")  # Mocks current datetime
+class TestCeleryIntegration(SimpleTestCase):
+    databases = "__all__"
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = CustomUser.objects.create_user(
+            email="email@email.com", username="name", password=make_password("password")
+        )
+        # Start celery worker
+        cls.celery_worker = start_worker(app, perform_ping_check=False)
+        cls.celery_worker.__enter__()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        # Close worker
+        cls.celery_worker.__exit__(None, None, None)
+
+    def _post_teardown(self):
+        # Clear Event instances after each test
+        Event.objects.all().delete()
+
+    def test_heartbeat_without_interval(self):
+        Event.objects.create(
+            title=f"Title-1", date="2020-01-01", time="10:00", user=self.user
+        )
+        self.assertEqual(Event.objects.count(), 1)
+        heartbeat_task = heartbeat.delay()  # Event should be deleted
+        result = heartbeat_task.get()
+        reschedule_or_delete_id, send_notification_id = result[0], result[1]
+        send_notification_task = AsyncResult(send_notification_id, app=app)
+        reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
+        send_notification_task.get()
+        reschedule_or_delete_task.get()
+        self.assertEqual(heartbeat_task.status, "SUCCESS")
+        self.assertEqual(send_notification_task.status, "SUCCESS")
+        self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
+        self.assertEqual(Event.objects.count(), 0)
+
+    def test_heartbeat_with_interval(self):
+        Event.objects.create(
+            title=f"Title-1",
+            date="2020-01-01",
+            time="10:00",
+            interval="30min",
+            user=self.user,
+        )
+        self.assertEqual(Event.objects.count(), 1)
+        heartbeat_task = heartbeat.delay()  # Event should be updated
+        result = heartbeat_task.get()
+        reschedule_or_delete_id, send_notification_id = result[0], result[1]
+        send_notification_task = AsyncResult(send_notification_id, app=app)
+        reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
+        send_notification_task.get()
+        reschedule_or_delete_task.get()
+        self.assertEqual(heartbeat_task.status, "SUCCESS")
+        self.assertEqual(send_notification_task.status, "SUCCESS")
+        self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.get().date, "2020-01-01")
+        self.assertEqual(Event.objects.get().time, "10:30")
+
+    def test_heartbeat_with_missed_timestamp(self):
+        Event.objects.create(
+            title=f"Title-1",
+            date="2019-01-01",
+            time="10:00",
+            interval="30min",
+            user=self.user,
+        )
+        self.assertEqual(Event.objects.count(), 1)
+        heartbeat_task = heartbeat.delay()  # Event should be updated
+        result = heartbeat_task.get()
+        reschedule_or_delete_id, send_notification_id = result[0], result[1]
+        send_notification_task = AsyncResult(send_notification_id, app=app)
+        reschedule_or_delete_task = AsyncResult(reschedule_or_delete_id, app=app)
+        send_notification_task.get()
+        reschedule_or_delete_task.get()
+        self.assertEqual(heartbeat_task.status, "SUCCESS")
+        self.assertEqual(send_notification_task.status, "SUCCESS")
+        self.assertEqual(reschedule_or_delete_task.status, "SUCCESS")
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.get().date, "2020-01-01")
+        self.assertEqual(Event.objects.get().time, "10:30")
+
+    def test_heartbeat_without_expired_events(self):
+        Event.objects.create(
+            title=f"Title-1", date="2020-01-01", time="11:00", user=self.user
+        )
+        self.assertEqual(Event.objects.count(), 1)
+        heartbeat_task = heartbeat.delay()  # Event should remain unchanged
+        result = heartbeat_task.get()
+        self.assertEqual(result, [])
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.get().date, "2020-01-01")
+        self.assertEqual(Event.objects.get().time, "11:00")

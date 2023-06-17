cb6330b0329f21aadc03fe1ca95228b2bbd28085
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-06-17 14:28:06 +0300

    add notification limits logic

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -5,9 +5,9 @@ from datetime import datetime, timedelta, timezone
 import requests
 from celery import chain, shared_task
 from django.core.mail import send_mail
+from django.conf import settings
 
 from core.models import Event, parse_notice_time_or_interval
-from users.models import UserSettings
 
 logger = logging.getLogger(__name__)
 
@@ -15,14 +15,23 @@ api_key = os.environ.get("VONAGE_API_KEY")
 api_secret = os.environ.get("VONAGE_API_SECRET")
 
 
-@shared_task()
-def send_windows_popup(title, text):  # For local Windows development only
-    import ctypes
-    import winsound
-
-    winsound.Beep(440, 1000)  # 440Hz, 1000ms
-    ctypes.windll.user32.MessageBoxW(0, text, title, 0)
-    return None
+def decrement_notifications_left(event):
+    notification_type = event.notification_type
+    notifications_left = getattr(event.user, f"{notification_type}_notifications_left")
+    if notifications_left == 1:
+        logger.info(f"Event {event} rejected due to notification limit")
+        notification_limit_message = f"Unfortunately you have no {notification_type} notifications left for this" \
+                                     f" month. Please contact {settings.CONTACT_EMAIL} if you would like to have this" \
+                                     f" limit lifted."
+        send_email_args_dict = {
+            "title": f"{notification_type} notification limit reached".capitalize(),
+            "text": notification_limit_message,
+            "email": event.user.email
+        }
+        send_email.delay(send_email_args_dict)
+    notifications_left -= 1
+    setattr(event.user, f"{event.notification_type}_notifications_left", notifications_left)
+    event.user.save()
 
 
 @shared_task()
@@ -100,19 +109,20 @@ notification_funcs = {"email": send_email, "sms": send_sms}
 def send_notification(event_pk):
     try:
         event = Event.objects.get(pk=event_pk)
-        user_settings = UserSettings.objects.get(user=event.user)
-        logger.info(f"{event} - Sending notification")
-        logger.info(f"{event.date} {event.time}")
-        notification_title, notification_text = build_notification_title_and_text(event)
-        args_dict = {
-            "title": notification_title,
-            "text": notification_text,
-            "email": event.user.email,
-            "phone_number": user_settings.phone_number,
-        }
-        notification_func = notification_funcs[event.notification_type]
-        notification_func.delay(args_dict)
-        # send_windows_popup.delay(notification_title, notification_text)
+        notification_type = event.notification_type
+        if getattr(event.user, f"{notification_type}_notifications_left") > 0:
+            logger.info(f"{event} - Sending notification")
+            logger.info(f"{event.date} {event.time}")
+            notification_title, notification_text = build_notification_title_and_text(event)
+            args_dict = {
+                "title": notification_title,
+                "text": notification_text,
+                "email": event.user.email,
+                "phone_number": event.user.phone_number,
+            }
+            notification_func = notification_funcs[notification_type]
+            notification_func.delay(args_dict)
+        decrement_notifications_left(event)
         return None
     except Exception as e:
         logger.exception(e)
@@ -137,6 +147,17 @@ def get_new_date_and_time(old_date, old_time, interval, current_utc_timestamp):
         raise
 
 
+def reschedule_event(event, current_utc_timestamp):
+    logger.info(f"{event} - Rescheduling")
+    new_date, new_time = get_new_date_and_time(
+        event.date, event.time, event.interval, current_utc_timestamp
+    )
+    event.date = new_date
+    event.time = new_time
+    logger.info(f"New: {event.date} {event.time}")
+    event.save()
+
+
 @shared_task()
 def reschedule_or_delete_event(event_pk, current_utc_timestamp):
     try:
@@ -145,14 +166,7 @@ def reschedule_or_delete_event(event_pk, current_utc_timestamp):
             logger.info(f"{event} - Deleting")
             event.delete()
         else:
-            logger.info(f"{event} - Rescheduling")
-            new_date, new_time = get_new_date_and_time(
-                event.date, event.time, event.interval, current_utc_timestamp
-            )
-            event.date = new_date
-            event.time = new_time
-            logger.info(f"New: {event.date} {event.time}")
-            event.save()
+            reschedule_event(event, current_utc_timestamp)
             return None
     except Exception as e:
         logger.exception(e)

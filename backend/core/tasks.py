1f435c0a4cf22b7889a34c6dd8ffee2b90f0ca5e
Zygimantas Milvydas <z.milvydas@gmail.com>
2023-09-13 17:36:23 +0300

    implement strategy pattern

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime, timedelta, timezone
 
+from abc import ABC, abstractmethod
 import requests
 from celery import shared_task
 from django.conf import settings
@@ -16,75 +17,33 @@ vonage_api_key = os.environ.get("VONAGE_API_KEY")
 vonage_api_secret = os.environ.get("VONAGE_API_SECRET")
 
 
-def decrement_notifications_left(event):
-    result = True
-    notification_type = event.notification_type
-    notifications_left = getattr(event.user, f"{notification_type}_notifications_left")
-    if notifications_left == 1:
-        logger.info(f"Event {event} rejected due to notification limit")
-        notification_limit_message = (
-            f"Unfortunately you have no {notification_type} notifications left for this"
-            f" month. Please contact {settings.CONTACT_EMAIL} if you would like to have this"
-            f" limit lifted.{settings.MESSAGE_SIGNATURE}"
-        )
-        send_email_args_dict = {
-            "title": f"{notification_type} notification limit reached".capitalize(),
-            "text": notification_limit_message,
-            "email": event.user.email,
-        }
-        send_email(send_email_args_dict)
-        result = False
-    notifications_left -= 1
-    setattr(
-        event.user, f"{event.notification_type}_notifications_left", notifications_left
-    )
-    event.user.save()
-    return result
-
-
-def send_sms(args_dict):
-    with requests.Session() as session:
-        phone_number = args_dict["phone_number"]
-        text = args_dict["text"]
-        url = "https://rest.nexmo.com/sms/json"
-        params = {
-            "api_key": vonage_api_key,
-            "api_secret": vonage_api_secret,
-            "from": "dont-forgetter",
-            "to": phone_number,
-            "text": text,
-        }
-        response = session.post(url, data=params)
-        logger.info(f"SMS response: {response.json()}")
-        if response.json()["messages"][0]["status"] == "0":
-            logger.info(f"SMS sent")
-            return True
-        else:
-            logger.warning("SMS sending failed")
-            return False
-
-
-def send_email(args_dict):
-    title = args_dict["title"]
-    text = args_dict["text"]
-    email = args_dict["email"]
-
-    result = send_mail(title, text, None, [email], fail_silently=False)
-    if result == 1:
-        logger.info("E-mail sent")
-        return True
-    logger.warning("E-mail sending failed")
-    return False
-
-
-class NotificationContents:
+class NotificationStrategy(ABC):
     def __init__(self, event):
         self.event = event
         if event.custom_variables:
             self.variable_dict = self.parse_custom_message_variables()
-
-        self.email_title = self.get_email_title()
         self.message = self.get_message()
+
+    @abstractmethod
+    def send_notification(self):
+        pass
+
+    def get_message(self):
+        if not self.event.custom_message:
+            return self.build_default_message()
+        elif self.event.custom_variables:
+            return self.build_custom_text(self.event.custom_message, self.variable_dict)
+        return self.event.custom_message
+
+    def build_custom_text(self, template, variable_dict):
+        """
+        Takes the template and replaces the plugs the variables in.
+        Example of template: 'hi {{name}}, your last name is {{surname}}'.
+        Example of output: 'hi Tom, your last name is Smith'.
+        """
+        for key, value in variable_dict.items():
+            template = template.replace("{{" + key + "}}", value)
+        return template
 
     def parse_custom_message_variables(self):
         """
@@ -98,24 +57,6 @@ class NotificationContents:
             variable_split = variable.split("=")
             variables_dict[variable_split[0].strip()] = variable_split[1]
         return variables_dict
-
-    def build_custom_text(self, template, variable_dict):
-        """
-        Takes the template and replaces the plugs the variables in.
-        Example of template: 'hi {{name}}, your last name is {{surname}}'.
-        Example of output: 'hi Tom, your last name is Smith'.
-        """
-        for key, value in variable_dict.items():
-            template = template.replace("{{" + key + "}}", value)
-        return template
-
-    def build_default_title(self):
-        notification_title = f"Time for {self.event.title}"
-        if self.event.category != "other":
-            notification_title += f" ({self.event.category})"
-        notification_title += "!"
-
-        return notification_title
 
     def build_default_message(self):
         notification_text = f"It is time for {self.event.title}"
@@ -133,96 +74,162 @@ class NotificationContents:
 
         return notification_text
 
-    def get_message(self):
-        if not self.event.custom_message:
-            return self.build_default_message()
-        elif self.event.custom_variables:
-            return self.build_custom_text(self.event.custom_message, self.variable_dict)
-        return self.event.custom_message
+
+class EmailNotification(NotificationStrategy):
+    def __init__(self, event):
+        super().__init__(event)
+        self.email_recipient = [self.event.user.email]
+        self.email_title = self.get_email_title()
+
+    def send_notification(self):
+        result = send_mail(self.email_title, self.message, None, self.email_recipient, fail_silently=False)
+        if result == 1:
+            logger.info("E-mail sent")
+            return True
+        logger.warning("E-mail sending failed")
+        return False
 
     def get_email_title(self):
-        if not self.event.custom_email_title:
+        if not self.event.custom_email_subject:
             return self.build_default_title()
         elif self.event.custom_variables:
-            return self.build_custom_text(self.event.custom_email_title, self.variable_dict)
-        return self.event.custom_email_title
+            return self.build_custom_text(self.event.custom_email_subject, self.variable_dict)
+        return self.event.custom_email_subject
+
+    def build_default_title(self):
+        notification_title = f"Time for {self.event.title}"
+        if self.event.category != "other":
+            notification_title += f" ({self.event.category})"
+        notification_title += "!"
+        return notification_title
 
 
-notification_funcs = {"email": send_email, "sms": send_sms}
+class SMSNotification(NotificationStrategy):
+    def __init__(self, event):
+        super().__init__(event)
+        self.sms_recipient = self.event.user.phone_number
 
-
-def send_notification(event):
-    notification_type = event.notification_type
-    if getattr(event.user, f"{notification_type}_notifications_left") > 0:
-        logger.info(f"{event} - Sending notification")
-        logger.info(f"{event.date} {event.time}")
-        notification_contents = NotificationContents(event)
-        args_dict = {
-            "title": notification_contents.email_title,
-            "text": notification_contents.message,
-            "email": event.user.email,
-            "phone_number": event.user.phone_number,
-        }
-        notification_func = notification_funcs[notification_type]
-        notification_sent = notification_func(args_dict)
-        if notification_sent:
-            if not event.user.premium_member:
-                decrement_notifications_left(event)
+    def send_notification(self):
+        with requests.Session() as session:
+            url = "https://rest.nexmo.com/sms/json"
+            params = {
+                "api_key": vonage_api_key,
+                "api_secret": vonage_api_secret,
+                "from": "dont-forgetter",
+                "to": self.sms_recipient,
+                "text": self.message,
+            }
+            response = session.post(url, data=params)
+            logger.info(f"SMS response: {response.json()}")
+            if response.json()["messages"][0]["status"] == "0":
+                logger.info(f"SMS sent")
                 return True
-        else:
-            if event.notification_retries_left > 0:
-                event.notification_retries_left -= 1
-                event.save()
+            else:
+                logger.warning("SMS sending failed")
                 return False
-            return True
-    return True
 
 
-def get_new_date_and_time(
-    old_date, old_time, interval, utc_offset, current_utc_timestamp
-):
-    current_datetime = datetime.fromtimestamp(current_utc_timestamp, tz=timezone.utc)
-    datetime_str = f"{old_date} {old_time}"
-    datetime_object = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M").replace(
-        tzinfo=timezone.utc
-    )
-    datetime_object = apply_utc_offset(utc_offset, datetime_object)
-    while datetime_object < current_datetime:
-        datetime_object += timedelta(**parse_notice_time_or_interval(interval))
-    datetime_object = apply_utc_offset(utc_offset, datetime_object, reverse=True)
-    datetime_str = datetime.strftime(datetime_object, "%Y-%m-%d %H:%M")
-    new_date = datetime_str[:10]
-    new_time = datetime_str[-5:]
-    return new_date, new_time
+class NotificationService:
+    def __init__(self, event):
+        self.event = event
+        if self.event.notification_type == "email":
+            strategy = EmailNotification
+        elif self.event.notification_type == "sms":
+            strategy = SMSNotification
+        else:
+            raise ValueError("Invalid notification preference")
+        self.strategy = strategy(self.event)
+
+    def send_notification(self):
+        notification_type = self.event.notification_type
+        if getattr(self.event.user, f"{notification_type}_notifications_left") > 0:
+            logger.info(f"{self.event} - Sending notification")
+            logger.info(f"{self.event.date} {self.event.time}")
+
+            notification_sent = self.strategy.send_notification()
+            if notification_sent:
+                if not self.event.user.premium_member:
+                    self.decrement_notifications_left()
+                    return True
+            else:
+                if self.event.notification_retries_left > 0:
+                    self.event.notification_retries_left -= 1
+                    self.event.save()
+                    return False
+                return True
+        return True
+
+    def decrement_notifications_left(self):
+        result = True
+        notifications_left = getattr(self.event.user, f"{self.event.notification_type}_notifications_left")
+        if notifications_left == 1:
+            logger.info(f"Event {self.event} rejected due to notification limit")
+            self.send_notification_limit_email()
+            result = False
+        notifications_left -= 1
+        setattr(
+            self.event.user, f"{self.event.notification_type}_notifications_left", notifications_left
+        )
+        self.event.user.save()
+        return result
+
+    def send_notification_limit_email(self):
+        notification_limit_message = (
+            f"Unfortunately you have no {self.event.notification_type} notifications left for this"
+            f" month. Please contact {settings.CONTACT_EMAIL} if you would like to have this"
+            f" limit lifted.{settings.MESSAGE_SIGNATURE}"
+        )
+        email_title = f"{self.event.notification_type} notification limit reached".capitalize()
+        result = send_mail(email_title, notification_limit_message, None, self.event.user.email, fail_silently=False)
+        if result == 1:
+            logger.info("Notification limit e-mail sent")
+        logger.warning("Notification limit e-mail sending failed")
 
 
-def reschedule_event(event, current_utc_timestamp):
-    logger.info(f"{event} - Rescheduling")
-    new_date, new_time = get_new_date_and_time(
-        event.date, event.time, event.interval, event.utc_offset, current_utc_timestamp
-    )
-    event.date = new_date
-    event.time = new_time
-    logger.info(f"New: {event.date} {event.time}")
-    event.notification_retries_left = settings.MAX_NOTIFICATION_RETRIES
-    event.save()
+class NotificationEvent:
+    def __init__(self, event, current_utc_timestamp):
+        self.event = event
+        self.current_utc_timestamp = current_utc_timestamp
 
+    def reschedule_or_delete_event(self):
+        if self.event.interval == "-":
+            logger.info(f"{self.event} - Deleting")
+            self.event.delete()
+        else:
+            self.reschedule_event()
 
-def reschedule_or_delete_event(event, current_utc_timestamp):
-    if event.interval == "-":
-        logger.info(f"{event} - Deleting")
-        event.delete()
-    else:
-        reschedule_event(event, current_utc_timestamp)
+    def reschedule_event(self):
+        logger.info(f"{self.event} - Rescheduling")
+        new_date, new_time = self.get_new_date_and_time()
+        self.event.date = new_date
+        self.event.time = new_time
+        logger.info(f"New: {self.event.date} {self.event.time}")
+        self.event.notification_retries_left = settings.MAX_NOTIFICATION_RETRIES
+        self.event.save()
+
+    def get_new_date_and_time(self):
+        current_datetime = datetime.fromtimestamp(self.current_utc_timestamp, tz=timezone.utc)
+        datetime_str = f"{self.event.date} {self.event.time}"
+        datetime_object = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M").replace(
+            tzinfo=timezone.utc
+        )
+        datetime_object = apply_utc_offset(self.event.utc_offset, datetime_object)
+        while datetime_object < current_datetime:
+            datetime_object += timedelta(**parse_notice_time_or_interval(self.event.interval))
+        datetime_object = apply_utc_offset(self.event.utc_offset, datetime_object, reverse=True)
+        datetime_str = datetime.strftime(datetime_object, "%Y-%m-%d %H:%M")
+        new_date = datetime_str[:10]
+        new_time = datetime_str[-5:]
+        return new_date, new_time
 
 
 @shared_task()
 def send_notification_and_reschedule_or_delete_event(event_pk, current_utc_timestamp):
     try:
         event = Event.objects.get(pk=event_pk)
-        not_to_be_retried = send_notification(event)
+        not_to_be_retried = NotificationService(event).send_notification()
         if not_to_be_retried:
-            reschedule_or_delete_event(event, current_utc_timestamp)
+            NotificationEvent(event, current_utc_timestamp).reschedule_or_delete_event()
         return None
     except Exception as e:
         logger.exception(e)
